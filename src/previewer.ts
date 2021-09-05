4de1bf9cc00ed968068a8a37890b28a7c8a2b8c3
Yangtang Wu <wuyangtang@live.com>
2021-09-05 17:40:27 +0800

    Set sail: 1.0.0

---
+++ src/previewer.ts
@@ -0,0 +1,33 @@
+import * as vscode from 'vscode';
+import TurndownService from 'turndown';
+
+import { log } from './logger';
+
+
+export class Html2MarkdownPreviewer {
+
+    private _context: vscode.ExtensionContext;
+    private turndown: TurndownService;
+
+    constructor(context: vscode.ExtensionContext) {
+        this._context = context;
+        this.turndown = new TurndownService();
+        log('Html2MarkdownPreviewer is created.');
+    }
+
+    showPreviewer() {
+        if (!vscode.window.activeTextEditor) {
+            vscode.window.showWarningMessage('There is no active editor');
+        } else {
+            const currentText = vscode.window.activeTextEditor.document.getText();
+            const md = this.turndown.turndown(currentText);
+            this.showInNewEditor(md);
+        }
+    }
+
+    private async showInNewEditor(content: string): Promise<void> {
+        const doc = await vscode.workspace.openTextDocument({ language: 'markdown', content });
+        // navigate to the new text document containing the markdown content
+        vscode.commands.executeCommand('vscode.open', doc.uri);
+    }
+}
